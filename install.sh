2e283718c78a5e41283f548f895486a8d83162fc
xycabcd <145047708+xycabcd@users.noreply.github.com>
2024-05-29 13:31:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ mkdir $@
 git clone https://github.com/Homebrew/brew $@/Homebrew
 ln -s $(realpath $@/Homebrew/bin/brew) $(realpath $@/bin/brew)
 echo "Homebrew now installed under $(realpath $@), add $(realpath $@)/bin:$(realpath $@)/sbin to PATH to use Homebrew"
+echo "IMPORTANT: DO NOT MOVE $@, DO NOT CHANGE $@'S OR ANY OF IT'S PARENT DIRECTORIES' NAME!"
