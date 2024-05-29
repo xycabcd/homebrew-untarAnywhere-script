874967af65e9b80bacbf0076b366381e543cb5b9
xycabcd <145047708+xycabcd@users.noreply.github.com>
2024-05-29 13:01:12 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if hash realpath 2>/dev/null
+then
+echo "NEED realpath"
+exit
+fi
+mkdir $@
+git clone https://github.com/Homebrew/brew $@/Homebrew
+ln -s $(realpath $@/Homebrew/bin/brew) $(realpath $@/bin/brew)
+echo "Homebrew now installed under $(realpath $@), add $(realpath $@)/bin:$(realpath $@)/sbin to PATH to use Homebrew"
